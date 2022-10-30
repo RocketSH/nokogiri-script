c627aec4b64fe433fa7a2049e144d2f572142496
digitaldiver <1119han@gmail.com>
2022-10-30 12:22:57 +0100

    specific input, output path

--- noko.rb
+++ noko.rb
@@ -1,21 +1,14 @@
 require 'nokogiri'
 require 'json'
 
-input = Dir['input/*.xml']
-input.each do |el|
-  filename = File.basename el
-  file = File.read el
-  xml = Nokogiri::Slop file
-  hash = parse_xml xml
-  json = JSON.pretty_generate hash
-  File.write(filename.json, json)
-end
-
+# define a HTML/XML parse method
 def parse_xml(file)
+  # get data from a parent tag
   file.search('div').map do |t|
     result = {
       title: t['title']
     }
+    # get data from child tags
     t.search('label').map do |l|
       name = l['for']
       label = l.text
@@ -25,4 +18,16 @@ def parse_xml(file)
 
     result
   end
+end
+
+# Able to process multi XMl files from input folder to output folder
+input = Dir['input/*.xml']
+input.each do |el|
+  file_name = File.basename(el, '.xml')
+  file = File.read el
+  xml = Nokogiri::Slop file
+  hash = parse_xml(xml)
+  json = JSON.pretty_generate hash
+
+  File.write("output/#{file_name}.json", json)
 end
