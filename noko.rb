367dd09caafbcfdb4c8d291534116a1a07f05ef0
digitaldiver <1119han@gmail.com>
2022-10-30 02:19:48 +0200

    WIP batch process files

--- noko.rb
+++ noko.rb
@@ -1,23 +1,28 @@
 require 'nokogiri'
+require 'json'
 
-file = File.read("./TrainingPreference.xml")
-doc = Nokogiri::Slop file
+input = Dir['input/*.xml']
+input.each do |el|
+  filename = File.basename el
+  file = File.read el
+  xml = Nokogiri::Slop file
+  hash = parse_xml xml
+  json = JSON.pretty_generate hash
+  File.write(filename.json, json)
+end
 
-def parse_xml(doc)
-  doc.search("div").map do |t|
+def parse_xml(file)
+  file.search('div').map do |t|
     result = {
-      title: t['title'] 
+      title: t['title']
     }
-
-    t.xpath("//label").map do |l|
+    t.search('label').map do |l|
       name = l['for']
       label = l.text
       result[:name] = name
-      result[:lable] = label
+      result[:label] = label
     end
 
     result
   end
 end
-
-puts parse_xml(doc)
