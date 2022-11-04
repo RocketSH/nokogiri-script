4e72806feda903a0a767b0f23c4ac000620d3dd6
digital-diver <digital-diver@protonmail.com>
2022-11-04 15:36:54 +0100

    Allow script to generate a two layers of json structure

--- noko.rb
+++ noko.rb
@@ -1,33 +1,62 @@
 require 'nokogiri'
 require 'json'
+require 'fileutils'
 
 # define a HTML/XML parse method
 def parse_xml(file)
-  # get data from a parent tag
-  file.search('div').map do |t|
-    result = {
-      title: t['title']
+  # first layer of json data
+  title = file.css('template div p').text 
+  order = title.split('.')[0].to_i
+
+  result = {
+    order: order,
+    title: title,
+    subtitle: file.css('template div div').first.text
+  }
+
+  # second layer of json data, an array of hashes
+  items = []
+  file.css('template div div div').map.with_index do |t, i|
+    item = {
+      id: i,
+      optionItemTitle: t['title']
     }
-    # get data from child tags
+
     t.search('label').map do |l|
       name = l['for']
       label = l.text
-      result[:name] = name
-      result[:label] = label
+      item[:optionName] = name
+      item[:optionLabel] = label
     end
-
-    result
+    items << item
   end
+  result[:checkboxes] = items
+  result
+end
+
+# copy origin folder, and save files into input folder
+files = Dir.glob('your-orgin-folder/*')
+FileUtils.mkdir('input') unless File.exists?('input')
+FileUtils.cp_r(files, 'input')
+
+# replace vue files' extension to xml
+Dir['input/*.vue'].each do |f|
+  FileUtils.mv f, "input/#{File.basename(f,'.*')}.xml"
 end
 
 # Able to process multi XMl files from input folder to output folder
-input = Dir['input/*.xml']
-input.each do |el|
-  file_name = File.basename(el, '.xml')
-  file = File.read el
-  xml = Nokogiri::Slop file
-  hash = parse_xml(xml)
-  json = JSON.pretty_generate hash
+json = []
+inputs = Dir['input/*.xml']
 
-  File.write("output/#{file_name}.json", json)
+inputs.each_with_index do |file, i|
+  # json[:id] = i
+  content = File.open file 
+  xml = Nokogiri::Slop content
+  hash = parse_xml(xml)
+
+  json << hash
 end
+
+# generate the final json file
+prettier_json = JSON.pretty_generate json
+File.write("the-final.json", prettier_json)
